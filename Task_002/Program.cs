25c6e0b65e5d7c19cded1f883cb1ec4a117584c3
Kirill91s <kirill2542@gmail.com>
2023-06-13 22:40:22 +0300

    task2

---
+++ Task_002/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+        // 2, 3, 7 -> 7
+        // 44 5 78 -> 78
+        // 22 3 9 -> 22
+        Console.Write("Введите число: "); // ввод значений
+        int numberA = Convert.ToInt32(Console.ReadLine()); // ЧТение целого числа
+        Console.Write("Введите число: "); // ввод значений
+        int numberB = Convert.ToInt32(Console.ReadLine()); // ЧТение целого числа
+        Console.Write("Введите число: "); // ввод значений
+        int numberC = Convert.ToInt32(Console.ReadLine()); // ЧТение целого числа
+
+        if (numberA < numberB)
+        {
+         numberA = numberB;
+        }
+         if (numberA < numberC)
+        {
+         numberA = numberC;
+        }
+        else
+        {
+        Console.WriteLine("Проверь числа");
+        }
+        Console.WriteLine("max : " + numberA);
+    }
+}
